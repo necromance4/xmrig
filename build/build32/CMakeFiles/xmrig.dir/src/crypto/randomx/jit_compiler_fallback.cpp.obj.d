6b65e27cfa6114893c628f2d75b4825bbd4297e9
necromance4 <temacmka123@gmail.com>
2021-06-26 10:52:17 +0300

    ignore build output

--- build/build32/CMakeFiles/xmrig.dir/src/crypto/randomx/jit_compiler_fallback.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/xmrig.dir/src/crypto/randomx/jit_compiler_fallback.cpp.obj: \
- C:/Users/kissm/source/repos/xmrig/src/crypto/randomx/jit_compiler_fallback.cpp
--- build/build64/CMakeFiles/xmrig.dir/src/AppManager.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/xmrig.dir/src/AppManager.cpp.obj: \
- C:/Users/kissm/source/repos/xmrig/src/AppManager.cpp
